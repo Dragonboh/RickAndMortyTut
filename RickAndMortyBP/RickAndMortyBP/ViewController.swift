cfcd02ce4b60ef604a83d86b139dbb43d76c7bbc
Bogdan Pankevych <>
2026-02-08 20:59:46 +0100

    Initial commit
    - Create basic project
    - Deleted main storyboards
    - Created window.

---
+++ RickAndMortyBP/RickAndMortyBP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMortyBP
+//
+//  Created by admin on 01.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
